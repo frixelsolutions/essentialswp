697e11f7cbc4f7142a50a14d4273cffe1e237b98
michaelstaake <michael@frixelsolutions.com>
2015-11-22 10:03:53 -0800

    1.1.0

--- woocommerce.php
+++ woocommerce.php
@@ -2,15 +2,16 @@
 
 <div class="wrapper"><!-- closed in footer.php -->
 	<div class="row">
-		<div class="col-md-8">
+		<div class="col-md-9">
 			<div class="content">
-				<?php woocommerce_content(); ?>
+				<div class="woocommerce_container">
+					<?php woocommerce_content(); ?>
+				</div>
 			</div>
 			
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
-
 <?php get_footer(); ?>
